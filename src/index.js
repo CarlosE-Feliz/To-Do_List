137cebaba98357637cb709b2920b62f8d417801c
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-08-18 10:16:58 -0600

    Update index.js
    Fix bugs

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 import './styles.css';
 
-function createTask () {
-const insert = document.getElementById('ul-list');
-var data = [
+function createTask() {
+  const insert = document.getElementById('ul-list');
+  const data = [
     {
-        description: "Go to the market",
-        completed: false,
-        index: null
+      description: 'Go to the market',
+      completed: false,
+      index: null,
     },
 
     {
-        description: "Buy milk",
-        completed: false,
-        index: null
+      description: 'Buy milk',
+      completed: false,
+      index: null,
     },
 
     {
-        description: "Buy a new pencil",
-        completed: false,
-        index: null
-    }
-]
-    for(let i = 0; i < data.length; i+=1){
-        data[i].index = i;
-        insert.innerHTML += `<li class="list-el main" id="${i}">`
-        +'<input type="checkbox" class="checkbox">'
-        +'<div class="label-div">'
-        +`<label class="label-info"> ${data[i].description}</label>`
-        +'</div>'
-        +'<p class="icon"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></p>'
-        +'</li>';
-    }
+      description: 'Buy a new pencil',
+      completed: false,
+      index: null,
+    },
+  ];
+  for (let i = 0; i < data.length; i += 1) {
+    data[i].index = i;
+    insert.innerHTML += `<li class="list-el main" id="${i}">`
+        + '<input type="checkbox" class="checkbox">'
+        + '<div class="label-div">'
+        + `<label class="label-info"> ${data[i].description}</label>`
+        + '</div>'
+        + '<p class="icon"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></p>'
+        + '</li>';
+  }
 }
 
-createTask ()
+createTask();
