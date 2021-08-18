a8f05ff48d3afa5b7db52687e92383381156fd81
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-08-18 08:55:01 -0600

    List sctructure
    Update items for list structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -10,7 +11,7 @@ module.exports = {
       },
   plugins: [
     new HtmlWebpackPlugin({
-   template: 'dist/index.html'
+      template: './src/index.html'
     }),
   ],
   output: {
