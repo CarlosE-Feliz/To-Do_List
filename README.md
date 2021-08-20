854e1c6c941f5f7e7f3e0ad6a3ea999caafe3f60
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-08-20 12:37:23 -0600

    Add remove & edit

--- README.md
+++ README.md
@@ -11,6 +11,20 @@ Set up a "To Do List" project
 
 [Live demo](https://xjokeer.github.io/To-Do_List/)
 
+# Functionality 
+## click once delete.
+![image](https://user-images.githubusercontent.com/74747182/130277540-ee16e37f-a166-4814-9a3c-6485768d033a.png)
+
+## click two times to edit
+![image](https://user-images.githubusercontent.com/74747182/130278490-1efd2279-cbd0-4ff5-b0ca-723d3144856b.png)
+## select the task that you want to delete or complete(when your task is complete you can choose if yoy just want delete that one)
+![image](https://user-images.githubusercontent.com/74747182/130278082-317c3454-099d-426b-94fe-6250f1416cb9.png)
+## Select all the task that are completed and click yo delete those
+![image](https://user-images.githubusercontent.com/74747182/130278372-842470e8-ee29-41c2-a288-8478aa28b70a.png)
+
+## Click here ![image](https://user-images.githubusercontent.com/74747182/130278627-9fba1b66-3fb4-4405-912d-1c06b8cf6e51.png) to add a new task
+![image](https://user-images.githubusercontent.com/74747182/130278542-2cc9efcc-47e4-4c71-b362-8325c1518e33.png)
+
 ## Built With
 
 - HTML
--- src/index.js
+++ src/index.js
@@ -1,38 +1,23 @@
 import Completed from './interactive';
+import AddRemove from './add&remove';
 import './styles.css';
 
-const data = JSON.parse(localStorage.getItem('Task')) || [
-  {
-    description: 'Go to the market',
-    completed: true,
-    index: null,
-  },
-
-  {
-    description: 'Buy milk',
-    completed: true,
-    index: null,
-  },
-
-  {
-    description: 'Buy a new pencil',
-    completed: true,
-    index: null,
-  },
-];
-
+const data = JSON.parse(localStorage.getItem('Task')) || [];
+AddRemove.Add(data);
 function createTask() {
   const insert = document.getElementById('ul-list');
   for (let i = 0; i < data.length; i += 1) {
-    const styles = (data[i].completed) ? 'label-true' : '';
+    const styles = (data[i].completed) ? 'label-true' : 'list-el';
     const check = (data[i].completed) ? 'checked' : '';
     data[i].index = i;
     insert.innerHTML += `<li class="list-el main ${styles}" id="li-${data[i].index}">`
         + `<input type="checkbox" class="checkbox" id="check-${data[i].index}" ${check}>`
         + `<div class="label-div" id="div-${data[i].index}">`
         + `<label class="label-info" id="label-${data[i].index}"> ${data[i].description}</label>`
+        + `<input type="text" id="input-${data[i].index}" value="${data[i].description}" autofocus autocomplete="off"  class="input-edit">`
         + '</div>'
-        + '<p class="icon"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></p>'
+        + `<p class="icon"><i class="fa fa-ellipsis-v open" id="open-${data[i].index}" aria-hidden="true"></i> `
+        + `<i class="fa fa-trash-o close" id="close-${data[i].index}" aria-hidden="true"></i></p>`
         + '</li>';
   }
   Completed.ToDoComplete(data);
