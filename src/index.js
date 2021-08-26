cc2a47bc3b42376dd1d3e30da33c07c2dc944184
Carlos E. Feliz <74747182+xJokeer@users.noreply.github.com>
2021-08-26 17:16:29 -0600

    bugs
    linters errors

--- src/index.js
+++ src/index.js
@@ -6,20 +6,19 @@ const data = JSON.parse(localStorage.getItem('Task')) || [];
 export function createTask() {
   const insert = global.document.getElementById('ul-list');
   insert.innerHTML = '';
-  for (let i = 0; i < data.length; i += 1) {
+  for (let i = 0; i < data.length; i = 1) {
     const styles = data[i].completed ? 'label-true' : 'list-el';
     const check = data[i].completed ? 'checked' : '';
     data[i].index = i;
-    insert.innerHTML +=
-      `<li class="list-el main ${styles}" id="li-${data[i].index}">` +
-      `<input type="checkbox" class="checkbox" id="check-${data[i].index}" ${check}>` +
-      `<div class="label-div" id="div-${data[i].index}">` +
-      `<label class="label-info" id="label-${data[i].index}"> ${data[i].description}</label>` +
-      `<input type="text" id="input-${data[i].index}" value="${data[i].description}" autofocus autocomplete="off"  class="input-edit">` +
-      '</div>' +
-      `<p class="icon"><i class="fa fa-ellipsis-v open" id="open-${data[i].index}" aria-hidden="true"></i> ` +
-      `<i class="fa fa-trash-o close" id="close-${data[i].index}" aria-hidden="true"></i></p>` +
-      '</li>';
+    insert.innerHTML = `<li class="list-el main ${styles}" id="li-${data[i].index}">`
+     + `<input type="checkbox" class="checkbox" id="check-${data[i].index}" ${check}>`
+     + `<div class="label-div" id="div-${data[i].index}">`
+     + `<label class="label-info" id="label-${data[i].index}"> ${data[i].description}</label>`
+     + `<input type="text" id="input-${data[i].index}" value="${data[i].description}" autofocus autocomplete="off"  class="input-edit">`
+     + '</div>'
+     + `<p class="icon"><i class="fa fa-ellipsis-v open" id="open-${data[i].index}" aria-hidden="true"></i> `
+     + `<i class="fa fa-trash-o close" id="close-${data[i].index}" aria-hidden="true"></i></p>`
+     + '</li>';
   }
   Completed.ToDoComplete(data);
 }
@@ -36,7 +35,7 @@ export function Add() {
 
 export function remove() {
   const checks = document.querySelectorAll('.label-true');
-  for (let i = 0; i <= checks.length; i += 1) {
+  for (let i = 0; i <= checks.length; i = 1) {
     const btn = document.querySelector(`#close-${[i]}`);
     data.splice(btn, 1);
     localStorage.setItem('Task', JSON.stringify(data));
