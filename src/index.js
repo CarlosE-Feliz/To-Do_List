be9ee62434cfd358861f3c9f68b0e08d433609a8
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-08-27 03:54:08 +0500

    Implement remove method

--- src/index.js
+++ src/index.js
@@ -7,18 +7,19 @@ export function createTask() {
   const insert = global.document.getElementById('ul-list');
   insert.innerHTML = '';
   for (let i = 0; i < data.length; i += 1) {
-    const styles = (data[i].completed) ? 'label-true' : 'list-el';
-    const check = (data[i].completed) ? 'checked' : '';
+    const styles = data[i].completed ? 'label-true' : 'list-el';
+    const check = data[i].completed ? 'checked' : '';
     data[i].index = i;
-    insert.innerHTML += `<li class="list-el main ${styles}" id="li-${data[i].index}">`
-        + `<input type="checkbox" class="checkbox" id="check-${data[i].index}" ${check}>`
-        + `<div class="label-div" id="div-${data[i].index}">`
-        + `<label class="label-info" id="label-${data[i].index}"> ${data[i].description}</label>`
-        + `<input type="text" id="input-${data[i].index}" value="${data[i].description}" autofocus autocomplete="off"  class="input-edit">`
-        + '</div>'
-        + `<p class="icon"><i class="fa fa-ellipsis-v open" id="open-${data[i].index}" aria-hidden="true"></i> `
-        + `<i class="fa fa-trash-o close" id="close-${data[i].index}" aria-hidden="true"></i></p>`
-        + '</li>';
+    insert.innerHTML +=
+      `<li class="list-el main ${styles}" id="li-${data[i].index}">` +
+      `<input type="checkbox" class="checkbox" id="check-${data[i].index}" ${check}>` +
+      `<div class="label-div" id="div-${data[i].index}">` +
+      `<label class="label-info" id="label-${data[i].index}"> ${data[i].description}</label>` +
+      `<input type="text" id="input-${data[i].index}" value="${data[i].description}" autofocus autocomplete="off"  class="input-edit">` +
+      '</div>' +
+      `<p class="icon"><i class="fa fa-ellipsis-v open" id="open-${data[i].index}" aria-hidden="true"></i> ` +
+      `<i class="fa fa-trash-o close" id="close-${data[i].index}" aria-hidden="true"></i></p>` +
+      '</li>';
   }
   Completed.ToDoComplete(data);
 }
@@ -34,13 +35,10 @@ export function Add() {
 }
 
 export function remove() {
-  const insert = document.getElementById('ul-list');
   const checks = document.querySelectorAll('.label-true');
   for (let i = 0; i <= checks.length; i += 1) {
     const btn = document.querySelector(`#close-${[i]}`);
-    console.log(btn);
     data.splice(btn, 1);
-    insert.removeChild(i);
     localStorage.setItem('Task', JSON.stringify(data));
   }
 }
@@ -70,18 +68,24 @@ function edit(e) {
       inputs.addEventListener('keyup', (e) => {
         const keycode = e.keyCode || e.which;
         if (keycode === 13) {
-          const newValue = o.parentElement.parentElement.querySelector('.input-edit').value;
-          const newLabels = o.parentElement.parentElement.querySelector('.label-info');
+          const newValue =
+            o.parentElement.parentElement.querySelector('.input-edit').value;
+          const newLabels =
+            o.parentElement.parentElement.querySelector('.label-info');
           newLabels.innerHTML = newValue;
           data[index].description = newValue;
           localStorage.setItem('Task', JSON.stringify(data));
         }
       });
-      o.parentElement.parentElement.querySelector('.open').style.display = 'none';
-      o.parentElement.parentElement.querySelector('.close').style.display = 'flex';
+      o.parentElement.parentElement.querySelector('.open').style.display =
+        'none';
+      o.parentElement.parentElement.querySelector('.close').style.display =
+        'flex';
     } else {
-      o.parentElement.parentElement.querySelector('.open').style.display = 'flex';
-      o.parentElement.parentElement.querySelector('.close').style.display = 'none';
+      o.parentElement.parentElement.querySelector('.open').style.display =
+        'flex';
+      o.parentElement.parentElement.querySelector('.close').style.display =
+        'none';
     }
   });
 }
